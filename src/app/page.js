10c4371bbce27731000cc540e8ab13d864303648
quoc-fe <quockoi31155@gmail.com>
2024-09-21 22:47:08 +0700

    đa

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
 "use client";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Loading from "../../public/loading.gif";
 import a1 from "../../public/1.gif";
 import a2 from "../../public/2.gif";
 import T from "../../public/typing.gif";
 import Countdown from "react-countdown";
 import emailjs from "emailjs-com";
+import LL from "../../public/IMG_5425 2.JPG";
 export default function Home() {
   const [login, setLogin] = useState(false);
+  const [lod, setLod] = useState(false);
+  const [con, setCon] = useState(false);
   const [click, setClick] = useState(false);
   function sendEmail() {
     //This is important, i'm not sure why, but the email won't send without it
@@ -24,6 +27,15 @@ export default function Home() {
     }
   }
   const form = useRef();
+  const lick = useRef(false);
+  useEffect(() => {
+    if (login) {
+      setTimeout(() => {
+        setLod(false);
+        setCon(true);
+      }, 6000);
+    }
+  }, [login]);
   return (
     <div className="h-screen flex items-center justify-center">
       <div class="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl">
@@ -42,6 +54,7 @@ export default function Home() {
                   className="w-[90%] rounded-full outline-none py-2 mb-5 text-center text-white bg-gradient-to-r from-[#FF7785] to-[#FF1691]"
                   onClick={() => {
                     setLogin(true);
+                    setLod(true);
                   }}
                 >
                   LOG IN TO SEE
@@ -52,106 +65,81 @@ export default function Home() {
               </div>
             </div>
           )}
-          {login && (
-            <Countdown
-              date={Date.now() + 6000}
-              renderer={({ hours, minutes, seconds, completed }) => {
-                if (completed) {
-                  // Render a completed state
-                  return (
-                    <div className="">
-                      <div className="absolute pt-[100px]  top-0 left-0 z-[100]  w-full h-full bg-[#FF9A9E]">
-                        <div class="flex mb-3 items-center space-x-2">
-                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
-                            <img
-                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
-                              alt=""
-                              class="h-8 w-8 object-fill rounded-full"
-                            />
-                          </div>
-                          <div class="block">
-                            <div class="flex justify-center items-center space-x-2">
-                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
-                                <div class="md:text-sm">
-                                  <Image src={a1} width={100} />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="flex mb-3 items-center space-x-2">
-                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
-                            <img
-                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
-                              alt=""
-                              class="h-8 w-8 object-fill rounded-full"
-                            />
-                          </div>
-                          <div class="block">
-                            <div class="flex justify-center items-center space-x-2">
-                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
-                                <div class="md:text-sm">
-                                  <Image src={a2} width={100} />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="flex mb-3 items-center space-x-2">
-                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
-                            <img
-                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
-                              alt=""
-                              class="h-8 w-8 object-fill rounded-full"
-                            />
-                          </div>
-                          <div class="block">
-                            <div class="flex justify-center items-center space-x-2">
-                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
-                                <div class="md:text-sm">
-                                  <span>
-                                    Cho phép tui được call với bà nha!{" "}
-                                  </span>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <button
-                          className="rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none"
-                          onClick={() => {
-                            sendEmail();
-                          }}
-                        >
-                          Đồng Ý
-                        </button>
-                        <br />
-                        <button
-                          className={`${
-                            click ? `bottom-[70%] left-[30%]` : ``
-                          } absolute z-[200]  mt-3 rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none`}
-                          onClick={() => {
-                            setClick(!click);
-                          }}
-                        >
-                          Không Đồng Ý
-                        </button>
+          <div className={con ? "block" : "hidden"}>
+            <div className="absolute pt-[100px]  top-0 left-0 z-[150]  w-full h-full bg-[#FF9A9E]">
+              <div class="flex mb-3 items-center space-x-2">
+                <div class="group relative flex flex-shrink-0 rounded-full self-start cursor-pointer">
+                  <Image src={LL} width={30} className="rounded-full" />
+                </div>
+                <div class="block">
+                  <div class="flex justify-center items-center space-x-2">
+                    <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                      <div class="md:text-sm">
+                        <Image src={a1} width={100} />
                       </div>
                     </div>
-                  );
-                } else {
-                  // Render a countdown
-                  return (
-                    <div className="absolute flex items-center top-0 left-0 z-[100]  w-full h-full bg-[#FF9A9E]">
-                      <div>
-                        <Image src={Loading} />
+                  </div>
+                </div>
+              </div>
+              <div class="flex mb-3 items-center space-x-2">
+                <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
+                  <Image src={LL} width={30} className="rounded-full" />
+                </div>
+                <div class="block">
+                  <div class="flex justify-center items-center space-x-2">
+                    <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                      <div class="md:text-sm">
+                        <Image src={a2} width={100} />
                       </div>
                     </div>
-                  );
-                }
-              }}
-            />
-          )}
+                  </div>
+                </div>
+              </div>
+              <div class="flex mb-3 items-center space-x-2">
+                <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
+                  <Image src={LL} width={30} className="rounded-full" />
+                </div>
+                <div class="block">
+                  <div class="flex justify-center items-center space-x-2">
+                    <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                      <div class="md:text-sm">
+                        <span>Cho phép tui được call với bà nha! </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button
+                className="rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none"
+                onClick={() => {
+                  sendEmail();
+                }}
+              >
+                Đồng Ý
+              </button>
+              <br />
+              <button
+                className={`${
+                  click ? `bottom-[70%] left-[30%]` : ``
+                } absolute z-[200]  mt-3 rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none`}
+                onClick={() => {
+                  setClick(!click);
+                }}
+              >
+                Không Đồng Ý
+              </button>
+            </div>
+          </div>
+          <div
+            className={`${
+              lod ? "flex" : "hidden"
+            } absolute  items-center top-0 left-0 z-[100]  w-full h-full bg-[#FF9A9E]`}
+          >
+            <div>
+              <Image src={Loading} />
+            </div>
+          </div>
+
           <form className="hidden" ref={form}>
             <label>Name</label>
             <input type="text" name="user_name" />
