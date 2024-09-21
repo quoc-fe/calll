2b622305bf4dee3ad6ac09b6246c18308af181f2
quoc-fe <quockoi31155@gmail.com>
2024-09-21 21:59:24 +0700

    dada

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,168 @@
+"use client";
 import Image from "next/image";
-
+import { useRef, useState } from "react";
+import Loading from "../../public/loading.gif";
+import a1 from "../../public/1.gif";
+import a2 from "../../public/2.gif";
+import T from "../../public/typing.gif";
+import Countdown from "react-countdown";
+import emailjs from "emailjs-com";
 export default function Home() {
+  const [login, setLogin] = useState(false);
+  const [click, setClick] = useState(false);
+  function sendEmail() {
+    //This is important, i'm not sure why, but the email won't send without it
+    try {
+      emailjs.sendForm(
+        "service_ki5ykv2",
+        "template_t84piag",
+        form.current,
+        "ysKcnErGp5FB9GlMt"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const form = useRef();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="h-screen flex items-center justify-center">
+      <div class="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl">
+        <div class="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
+        <div class="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+        <div class="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+        <div class="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+        <div class="relative rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
+          {!login && (
+            <div className="flex items-end justify-center w-full h-full">
+              <div className="w-full text-center ">
+                <div className="mb-[100px]">
+                  <img src="/halo.gif" />
+                </div>
+                <button
+                  className="w-[90%] rounded-full outline-none py-2 mb-5 text-center text-white bg-gradient-to-r from-[#FF7785] to-[#FF1691]"
+                  onClick={() => {
+                    setLogin(true);
+                  }}
+                >
+                  LOG IN TO SEE
+                </button>
+                <p className="text-[#B3B3BF]/80 text-center py-2 border-t text-[13px] w-full">
+                  Create by Quoc....
+                </p>
+              </div>
+            </div>
+          )}
+          {login && (
+            <Countdown
+              date={Date.now() + 6000}
+              renderer={({ hours, minutes, seconds, completed }) => {
+                if (completed) {
+                  // Render a completed state
+                  return (
+                    <div className="">
+                      <div className="absolute pt-[100px]  top-0 left-0 z-[100]  w-full h-full bg-[#FF9A9E]">
+                        <div class="flex mb-3 items-center space-x-2">
+                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
+                            <img
+                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
+                              alt=""
+                              class="h-8 w-8 object-fill rounded-full"
+                            />
+                          </div>
+                          <div class="block">
+                            <div class="flex justify-center items-center space-x-2">
+                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                                <div class="md:text-sm">
+                                  <Image src={a1} width={100} />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex mb-3 items-center space-x-2">
+                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
+                            <img
+                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
+                              alt=""
+                              class="h-8 w-8 object-fill rounded-full"
+                            />
+                          </div>
+                          <div class="block">
+                            <div class="flex justify-center items-center space-x-2">
+                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                                <div class="md:text-sm">
+                                  <Image src={a2} width={100} />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex mb-3 items-center space-x-2">
+                          <div class="group relative flex flex-shrink-0 self-start cursor-pointer">
+                            <img
+                              src="https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_1280.jpg"
+                              alt=""
+                              class="h-8 w-8 object-fill rounded-full"
+                            />
+                          </div>
+                          <div class="block">
+                            <div class="flex justify-center items-center space-x-2">
+                              <div class="bg-gray-100 w-auto rounded-xl px-2 pb-2">
+                                <div class="md:text-sm">
+                                  <span>
+                                    Cho phép tui được call với bà nha!{" "}
+                                  </span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <button
+                          className="rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none"
+                          onClick={() => {
+                            sendEmail();
+                          }}
+                        >
+                          Đồng Ý
+                        </button>
+                        <br />
+                        <button
+                          className={`${
+                            click ? `bottom-[70%] left-[30%]` : ``
+                          } absolute z-[200]  mt-3 rounded-full p-4 px-6 bg-[#E0AD82] text-white outline-none`}
+                          onClick={() => {
+                            setClick(!click);
+                          }}
+                        >
+                          Không Đồng Ý
+                        </button>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  // Render a countdown
+                  return (
+                    <div className="absolute flex items-center top-0 left-0 z-[100]  w-full h-full bg-[#FF9A9E]">
+                      <div>
+                        <Image src={Loading} />
+                      </div>
+                    </div>
+                  );
+                }
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          )}
+          <form className="hidden" ref={form}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
